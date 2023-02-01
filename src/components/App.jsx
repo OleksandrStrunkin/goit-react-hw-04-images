e11d069e4aa2d71f193fbdd01cbf1610e07c3099
OleksandrStrunkin <oleksandr.strunkin@gmail.com>
2023-02-01 14:10:19 +0200

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -59,7 +59,6 @@ export default function App() {
   return (
     <div className={css.App}>
       <Searchbar onSubmit={onSubmit} />
-      {error && <p>{error}</p>}
       {loading && <Audio />}
       <ImageGallery items={items} openModal={openModal} />
       {Boolean(items.length) && <Button onLoadMore={loadMore} />}
